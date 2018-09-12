e540b2cbb21d1fe5fef703b26e5bf9a88875326c
tekraj <tekrajpant224@gmail.com>
2018-09-12 23:11:25 +0545

    gallery page design completed

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  title = 'Gallery';
+  public innerHeight : number;
+  public background:string;
+  public dateTime:string;
+  constructor() { 
+    let now = new Date();
+    this.dateTime = now.getDate()+'.'+(now.getMonth()+1)+'.'+now.getFullYear();
+    this.background = '../assets/images/gallery-background.png';
   }
 
+  ngOnInit() {
+    this.innerHeight = window.innerHeight;
+  }
+  public getArrayFromNumber (num:number){
+    let numberArray = [];
+    for(let i=1;i<=num;i++){
+      numberArray.push(i);
+    }
+    return numberArray;
+  }
 }
